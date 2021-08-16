e0e8ceb00fb9d7a2ad94d4c78b666bb2aeca7962
Thomas <thomasklein18@gmail.com>
2021-08-16 21:34:53 +0200

    movement

--- maze/Assets/Scripts/Player.cs
+++ maze/Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,9 +13,6 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float pitch = -1 * Input.GetAxis("Vertical");
-        float roll = -1 * Input.GetAxis("Horizontal");
-        this.transform.Rotate(pitch, 0, roll, Space.Self);
-    
+        
     }
 }
