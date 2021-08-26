f1bf64732be6038b1b712ed38ebe31d5f459dfc6
Thomas <thomasklein18@gmail.com>
2021-08-26 12:28:20 +0200

    improve see-through logic

--- maze/Assets/Scripts/Controller.cs
+++ maze/Assets/Scripts/Controller.cs
@@ -13,28 +13,13 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
     }
 
     void FixedUpdate() {
-        
+    
         ControlRotation();
 
-        // // rotates kinematic rigidbodys, but they rotate independently around their centers
-        // // foreach(Rigidbody rb in this.GetComponentsInChildren<Rigidbody>()) {
-        // //     Quaternion delta = Quaternion.Euler(roll/5, yaw/5, pitch/5);
-        // //     rb.MoveRotation(rb.rotation * delta);
-        // // }
-
-        // // will rotate the parent as intended (ie rotates all children around center) but ball falls through
-        // // 
-        
-        // Quaternion delta = Quaternion.Euler(roll, yaw, pitch);
-        // Rigidbody rb = this.GetComponent<Rigidbody>();
-        // rb.MoveRotation(rb.rotation * delta);
-        // // Vector3 torque = transform.up * pitch * 1000f;
-        // // rb.AddTorque(torque);
-        // // Debug.Log("Torque: " + torque);
     }
 
     void ControlRotation() {
@@ -42,6 +27,8 @@ public class Controller : MonoBehaviour
         float pitch = Input.GetAxis("Pitch");
         float yaw = Input.GetAxis("Yaw"); 
 
-        this.transform.Rotate(roll/5, yaw/5, pitch/5, Space.World);
+        float damp = 0.3f; // dampening factor to slow down movement enough to maintain proper collisions
+
+        this.transform.Rotate(roll*damp, yaw*damp, pitch*damp, Space.World);
     }
 }
--- maze/Assets/Scripts/SeeThrough.cs
+++ maze/Assets/Scripts/SeeThrough.cs
@@ -6,8 +6,8 @@ public class SeeThrough : MonoBehaviour
 {
 
     [SerializeField] GameLogic gameLogic;
-
     private Renderer rend;
+    private bool ready = true; // whether this wall is ready to be rendered again
 
 
     // Start is called before the first frame update
@@ -15,7 +15,6 @@ public class SeeThrough : MonoBehaviour
     {
         rend = this.GetComponent<Renderer>();
         rend.enabled = true;
-        
     }
 
     // Update is called once per frame
@@ -26,14 +25,30 @@ public class SeeThrough : MonoBehaviour
         bool hit = Physics.Raycast(Camera.main.transform.position, dir, out hitInfo, dir.magnitude);
         
         if(hit) {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitInfo.distance, Color.yellow);
             GameObject wall = hitInfo.collider.gameObject;
             if( wall == this.gameObject) {
-                SetVisibility(false);
+                if(this.rend.enabled) SetInvisible();
             } else {
-                SetVisibility(true);
+                if(ready) rend.enabled = true;
             }
         }
+    }
+
+    void SetInvisible() {
+        rend.enabled = false;
+        ready = false;
+        StartCoroutine(FadeBackIn());
+    }
+
+    protected IEnumerator FadeBackIn()
+    {
+        yield return new WaitForSeconds(2);
+        this.ready = true;
+    }
+
+    void SetVisible() {
+        rend.enabled = true;
+
     }
 
     void SetVisibility(bool visible) {
