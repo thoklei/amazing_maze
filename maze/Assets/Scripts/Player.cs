5e20c72c3fb5867ea012493d55dd0f014294920c
ldankert <leon.dankert@uni-osnabrueck.de>
2021-09-02 11:27:13 +0200

    First killer script and player respawn

--- maze/Assets/Scripts/Player.cs
+++ maze/Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform respawnPoint;
+    private bool dead = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,16 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Check if dead and ifso transform player to respawnpoint
+       if(dead)
+        {
+            this.transform.position = respawnPoint.transform.position;
+            dead = false;
+        }
+    }
+
+    public void KillPlayer()
+    {
+        dead = true;
     }
 }
