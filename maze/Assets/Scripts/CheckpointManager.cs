d54ddf36a25c205433054cfc768eee38a1896432
chbroecker <broecker.christopher@gmail.com>
2021-09-26 15:05:12 +0200

    fix wrong checkpoint

--- maze/Assets/Scripts/CheckpointManager.cs
+++ maze/Assets/Scripts/CheckpointManager.cs
@@ -16,6 +16,7 @@ public class CheckpointManager : MonoBehaviour
             if (checkpoint.activated)
             {
                 _latestCheckpoint = checkpoint;
+                
             }
         }
     }
